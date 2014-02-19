993a8d2262fe11b1943fa4b675a267cdfab26784
Robert Leahy <rleahy@rleahy.ca>
2014-02-19 09:41:53 -0800

    IE 7 DOM Fix
    In IE 7 elements pulled from the DOM aren't of the correct type, so adding member functions to the prototypes of the correct objects has no effect.
    
    Accordingly, turned the DOM utilities into static functions.

--- src/dom.js
+++ src/dom.js
@@ -1,75 +1,55 @@
-//	Function so we have a scope
-(function () {
+if (typeof CivicInfoBC==='undefined') CivicInfoBC={};
 
-	var add=function (what, add) {
-	
-		for (var i=2;i<arguments.length;++i)
-		if (!(
-			(arguments[i]===null) ||
-			arguments[i].prototype[what]
-		)) arguments[i].prototype[what]=add;
-	
-	};
-	
-	var html=(typeof HTMLElement==='undefined') ? null : HTMLElement;
-	var e=(typeof Element==='undefined') ? null : Element;
-	var node=(typeof Node==='undefined') ? null : Node;
-	
-	add(
-		'hasClass',
-		function (name) {
-		
-			var regex=new RegExp('(?:^|\\s)'+RegExp.escape(name)+'(?:\\s|$)');
-			
-			return regex.test(this.className);
-		
-		},
-		e,
-		node
-	);
-	
-	add(
-		'addClass',
-		function (name) {
-		
-			if (!this.hasClass(name)) this.className+=' '+name;
-		
-		},
-		e,
-		node
-	);
-	
-	add(
-		'removeClass',
-		function (name) {
-		
-			var regex=new RegExp(
-				'(?:^|\\s)'+RegExp.escape(name)+'(?=\\s|$)',
-				'g'
-			);
-			
-			this.className=this.className.replace(regex,'');
-		
-		},
-		e,
-		node
-	);
-	
-	add(
-		'getElementsByClassName',
-		function (name) {
-	
-			var elements=this.getElementsByTagName('*');
-			var retr=new Array();
-			
-			for (var i=0;i<elements.length;++i) if (elements[i].hasClass(name)) retr.push(elements[i]);
-			
-			return retr;
-		
-		},
-		html,
-		e,
-		node
-	);
 
-})();
+CivicInfoBC.DOM={};
+
+
+CivicInfoBC.DOM.HasClass=function (e, name) {
+
+	var regex=new RegExp(
+		'(?:^|\\s)'+
+		RegExp.escape(name)+
+		'(?:\\s|$)'
+	);
+	
+	return regex.test(e.className);
+
+};
+
+
+CivicInfoBC.DOM.AddClass=function (e, name) {
+
+	if (!CivicInfoBC.DOM.HasClass(e,name)) e.className+=' '+name;
+
+};
+
+
+CivicInfoBC.DOM.RemoveClass=function (e, name) {
+
+	var regex=new RegExp(
+		'(?:^|\\s)'+RegExp.escape(name)+'(?=\\s|$)',
+		'g'
+	);
+	
+	e.className=e.className.replace(regex,'');
+
+};
+
+
+CivicInfoBC.DOM.GetElementsByClassName=function (e, name) {
+
+	if (typeof e.getElementsByClassName==='function') return e.getElementsByClassName(name);
+	
+	var elements=e.getElementsByTagName('*');
+	var retr=new Array();
+	
+	for (var i=0;i<elements.length;++i) if (
+		CivicInfoBC.DOM.HasClass(
+			elements[i],
+			name
+		)
+	) retr.push(elements[i]);
+	
+	return retr;
+
+};
