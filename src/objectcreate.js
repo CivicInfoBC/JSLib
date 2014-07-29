e94f879ef033ee878cec1752cce66c68bafb18ba
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2014-07-28 23:24:51 -0700

    Object.create Polyfill
    Added a polyfill (lifted almost exactly from MDN) for Object.create.

---
+++ src/objectcreate.js
@@ -0,0 +1,16 @@
+if (typeof Object.create!=='function') (function () {
+
+	var f=function () {	};
+	
+	Object.create=function (o) {
+	
+		if (arguments.length>1) throw Error('Second argument not supported');
+		if (o===null) throw Error('Cannot set a null [[Prototype]]');
+		if (typeof o!=='object') throw TypeError('Argument must be an object');
+		
+		f.prototype=o;
+		return new f();
+	
+	}
+
+})();
